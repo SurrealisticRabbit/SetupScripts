a191263687c5568a18a467d3c3c2009e136460b7
Ducky <no@thankyou.com>
2026-01-20 02:37:25 -0700

    fix reboot issue

--- scripts/04-configure.sh
+++ scripts/04-configure.sh
@@ -27,7 +27,6 @@ chattr +C /swapfile
 dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
 chmod 600 /swapfile
 mkswap /swapfile
-swapon /swapfile
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
 ### USERS
